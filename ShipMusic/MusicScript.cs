75379941478948d5debf43328ec649983bf9919a
ever39 <matei.morcovei@gmail.com>
2024-04-21 17:39:05 +0300

    made the mod!

---
+++ ShipMusic/MusicScript.cs
@@ -0,0 +1,71 @@
+﻿using LCSoundTool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ShipMusic
+{
+    internal class MusicScript : MonoBehaviour
+    {
+        public static GameObject hangarShip;
+        public static GameObject musicObj;
+        private static AudioClip musicClip = SoundTool.GetAudioClip("Command293-ShipMusic", "music.mp3");
+        public static AudioSource shipMusicSource;
+
+        private void Update()
+        {
+            try
+            {
+                if (hangarShip == null)
+                {
+                    hangarShip = GameObject.Find("/Environment/HangarShip/");
+                    musicClip = SoundTool.GetAudioClip("Command293-ShipMusic", "music.mp3");
+
+                    if (hangarShip != null)
+                    {
+                        musicObj = new GameObject("ShipMusicSource");
+                        musicObj.transform.SetParent(hangarShip.transform, false);
+                        musicObj.transform.rotation = Quaternion.identity;
+                        musicObj.transform.localScale = Vector3.one;
+                        musicObj.transform.localPosition = new Vector3(2.2f, 0.5f, -6.7f);
+                        musicObj.AddComponent<AudioSource>();
+
+                        shipMusicSource = musicObj.GetComponent<AudioSource>();
+
+                        shipMusicSource.dopplerLevel = 0;
+                        shipMusicSource.spatialBlend = 1;
+                        shipMusicSource.volume = Plugin.musicVolume.Value;
+                        shipMusicSource.loop = true;
+                        shipMusicSource.priority = 0;
+                        shipMusicSource.playOnAwake = false;
+                        shipMusicSource.maxDistance = Plugin.maxMusicDistance.Value;
+                        shipMusicSource.minDistance = 1;
+                        shipMusicSource.rolloffMode = AudioRolloffMode.Linear;
+                        shipMusicSource.clip = musicClip;
+                        shipMusicSource.Play();
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Plugin.mls.LogError($"found exception: {e}");
+            }
+        }
+
+        /*private void LateUpdate()
+        {
+            if(hangarShip == null | shipMusicSource.clip == null)
+            {
+                hangarShip = GameObject.Find("/Environment/HangarShip/");
+                shipMusicSource.clip = musicClip;
+            }
+            if(hangarShip != null && shipMusicSource.clip != null)
+            {
+                shipMusicSource.Play();
+            }
+        }*/
+    }
+}
--- ShipMusic/Plugin.cs
+++ ShipMusic/Plugin.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using LCSoundTool;
+using UnityEngine.SceneManagement;
 
 namespace ShipMusic
 {
-    public class Plugin
+    [BepInPlugin(GUID,NAME,VERSION)]
+    public class Plugin : BaseUnityPlugin
     {
+        public const string GUID = "command.ShipMusic";
+        public const string NAME = "ShipMusic";
+        public const string VERSION = "1.0.0";
+        public static AudioClip selectedClip;
+        public static ConfigEntry<float> musicVolume;
+        public static ConfigEntry<float> maxMusicDistance;
+        public static ManualLogSource mls { get; private set; }
+        private readonly Harmony har = new Harmony(GUID);
+
+        private void Awake()
+        {
+            mls = Logger;
+            musicVolume = Config.Bind("General", "Volume", 2f, "be glad that i added this");
+            maxMusicDistance = Config.Bind("General", "MaxDistance", 25f, "max distance of the sound");
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode loadMode)
+        {
+            if(scene.name == "SampleSceneRelay")
+            {
+                GameObject entireMod = new GameObject("ShipMusicController");
+                entireMod.AddComponent<MusicScript>();
+                DontDestroyOnLoad(entireMod);
+                entireMod.hideFlags = HideFlags.HideAndDontSave;
+                mls.LogInfo("added the music to the ship!");
+            }
+            if(scene.name == "MainMenu")
+            {
+                GameObject entireMod = GameObject.Find("ShipMusicController");
+                GameObject.Destroy(entireMod);
+                mls.LogInfo("in main menu, destroyed music object");
+            }
+        }
     }
 }
